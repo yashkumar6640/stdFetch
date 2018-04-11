413c9d0e94623e49c3bff20dd1e3cfab277cb029
Yash Kumar <yashkumar6640@gmail.com>
2018-04-11 17:34:04 +0530

    Update crudOperations.js

--- crudOperations.js
+++ crudOperations.js
@@ -1,4 +1,4 @@
-function crudOperations() {
+(function crudOperations() {
   var stdFetch = {
     request: function(url, options) {
       if (options.dataType === "json") {
@@ -59,8 +59,8 @@ function crudOperations() {
     }
   };
 
-  global.stdFetch = stdFetch;
+  window.stdFetch = stdFetch;
   
-}
+})()
 
 
